9459bd7ee266926c54c03bca5ab6dcf7fd1bc083
vzgv0420 <176408941+vzgv0420@users.noreply.github.com>
2025-07-03 11:16:54 +0000

    Create pseudocode

---
+++ src/softdevcourse2025/tri.py
@@ -0,0 +1,13 @@
+# Module to calculate hypot of triangle 
+
+# Hypot calc function
+# takes lengths a and b (opposite and adjacent) of a triangle
+#returns c, the hypotenuse 
+
+# Squaring function
+# takes length a
+# returns a^2
+
+# Adding function 
+# takes lengths a and b
+# returns a+b
